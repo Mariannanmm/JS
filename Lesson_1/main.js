a5c9b3025ecf47624d230ad558c7ab1ff81b2f4d
Mariannanmm <Marianna.nuk@gmail.com>
2025-08-06 12:56:34 +0300

    lesson_1_2

---
+++ Lesson_1/main.js
@@ -0,0 +1,13 @@
+//{
+//    let a = 'Hello Okten';
+ //   var b = 'Hello World';
+//}
+//console.log(b);
+//console.log(a) ;
+
+const PI = 3.141592652;
+console.log(PI) ;
+
+let a = 'qwerty' ;
+a = 'okten';
+console.log(a) ;
