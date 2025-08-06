5ab430b6ff7520c6ef5313ff12b9415d314af480
Mariannanmm <Marianna.nuk@gmail.com>
2025-08-06 13:34:45 +0300

    lesson_1_3

---
+++ Lesson_1/main_1_4.js
@@ -0,0 +1,11 @@
+//let a = "'asd'";
+//let b = '"asd"';
+//console.log(a, b);
+
+let greeting = 'Hello ';
+let intro = 'my name is vasya';
+let msg = greeting+intro;
+console.log(msg);
+
+let msg2 = `${greeting} ${intro}`;
+console.log(msg2);
