1c952ad9e2fc197b252d3e639993a223ae02f0c0
Mariannanmm <Marianna.nuk@gmail.com>
2025-09-05 14:38:48 +0300

    changes2_1

---
+++ Lesson_2/Homework_2/HW_2_1/HW_2_1.js
@@ -0,0 +1,22 @@
+let myArray = [
+    'hello',
+    42,
+    true,
+    null,
+    undefined,
+    {name: 'Anna'},
+    [1, 2, 3],
+    function() { return 'JS'; },
+    'world',
+    false
+];
+console.log(myArray[0]);
+console.log(myArray[1]);
+console.log(myArray[2]);
+console.log(myArray[3]);
+console.log(myArray[4]);
+console.log(myArray[5]);
+console.log(myArray[6]);
+console.log(myArray[7]);
+console.log(myArray[8]);
+console.log(myArray[9]);
