aec4f61a141a8fc3b70a27b5af6ab4255a5eab9e
Mariannanmm <Marianna.nuk@gmail.com>
2025-09-05 13:31:40 +0300

    changes_1_1

---
+++ Lesson_1/Homework_1/HW_1_1/HW_1_1.js
@@ -0,0 +1,38 @@
+let hello = 'hello';
+console.log(hello);
+
+let owu = 'owu';
+console.log(owu);
+
+let com = 'com';
+console.log(com);
+
+let ua = 'ua';
+console.log(ua);
+
+let a = 1;
+console.log(a);
+
+let b = 10;
+console.log(b);
+
+let c = -999;
+console.log(c);
+
+let d = 123;
+console.log(d);
+
+let Pi = 3.14;
+console.log(Pi);
+
+let f = 2.7;
+console.log(f);
+
+let g = 16;
+console.log(g);
+
+let h = true;
+console.log(h);
+
+let j = false;
+console.log(j);
