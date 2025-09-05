fce8d2486ae51716a13bff839c904f313e800981
Mariannanmm <Marianna.nuk@gmail.com>
2025-09-05 17:47:51 +0300

    changes7

---
+++ Lesson_8/lesson_8_3/8_3.js
@@ -0,0 +1,24 @@
+//let user = {
+//    name: "January",
+//    age: 25,
+//}
+
+//let user2 = user;
+//console.log(user === user2);
+
+//let user = {
+//    name:'John',
+//    age:45
+//}
+//let userCopy = {...user};
+//console.log(userCopy === user);
+//userCopy.age = 40;
+//console.log(userCopy);
+//console.log(user);
+
+let nums = [1, 2, 3, 4, 5];
+let nums2 = nums;
+console.log(nums === nums2);
+
+
+
