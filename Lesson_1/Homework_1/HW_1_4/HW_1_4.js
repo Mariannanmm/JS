d1104c659fa3b9651aac9b7bcc2690ad45d32f1c
Mariannanmm <Marianna.nuk@gmail.com>
2025-09-05 14:10:32 +0300

    changes_1_4

---
+++ Lesson_1/Homework_1/HW_1_4/HW_1_4.js
@@ -0,0 +1,8 @@
+let firstName = prompt("Enter your first name:");
+let middleName = prompt("Enter your middle name:");
+let age = prompt("Enter your age number:");
+
+
+console.log("first name:", firstName);
+console.log("middle name:", middleName);
+console.log("age:", age);
