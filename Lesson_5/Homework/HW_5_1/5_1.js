c00e873e99aa686caf7868156314befad787eb33
Mariannanmm <Marianna.nuk@gmail.com>
2025-09-05 15:24:16 +0300

    changes5_1-2

---
+++ Lesson_5/Homework/HW_5_1/5_1.js
@@ -0,0 +1,10 @@
+const calculateRectangleArea = (a, b) => a * b;
+
+// Приклад:
+let sideA = 5;
+let sideB = 10;
+
+let area = calculateRectangleArea(sideA, sideB);
+
+console.log(`Площа прямокутника зі сторонами ${sideA} і ${sideB} дорівнює: ${area}`);
+// Виведе: Площа прямокутника зі сторонами 5 і 10 дорівнює: 50
---
+++ Lesson_5/Homework/HW_5_2/5_2.js
@@ -0,0 +1,7 @@
+const calculateCircleArea = (r) => Math.PI * r * r;
+
+// Приклад:
+let radius = 7;
+let area = calculateCircleArea(radius);
+
+console.log(`Площа кола з радіусом ${radius} дорівнює: ${area}`);
