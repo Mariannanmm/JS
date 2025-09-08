3107827a9dff12c4ca68b5322f9c9976da22ab75
Mariannanmm <Marianna.nuk@gmail.com>
2025-09-08 13:57:07 +0300

    lesson 8_4

---
+++ Lesson_8/lesson_8_4/8_4.js
@@ -0,0 +1,20 @@
+let user = {
+    name: "John",
+    skills: ['html', 'javascript'],
+}
+//let userClone = {...user};
+//console.log(user === userClone);
+//console.log(user.skills === userClone.skills);
+
+//let s = JSON.stringify(123);
+//console.log(s);
+//console.log(typeof s);
+
+let JSON = {
+    name: "Johnny",
+    skills: ['html', 'javascript'],
+}
+console.log(JSON);
+
+//let parse = JSON.parse(user);
+//console.log(parse);
