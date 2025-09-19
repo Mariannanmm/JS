e925b9b6337fea607838888710d2b748a1f17366
Mariannanmm <Marianna.nuk@gmail.com>
2025-09-19 17:52:22 +0300

    lesson 12 4

---
+++ Lesson_12/lesson_12_4/12_4.js
@@ -0,0 +1,25 @@
+//setTimeout(() => {
+//    let x = 1;
+//    console.log(x);
+//    setTimeout(() => {
+//        x++;
+//        console.log(x);
+//    }, 1000);
+//}, 1000);
+
+
+new Promise((resolve) => {
+    setTimeout(() => {
+    let x = 1;
+    console.log(x);
+    resolve(x);
+    },1000)
+})
+    .then((x) => {
+    new Promise((resolve) => {
+        setTimeout(() => {
+            x++;
+            console.log(x);
+        },1000)
+    })
+})
